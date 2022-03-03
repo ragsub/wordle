756e8d3fcde2291c0d027eb7dd33ed62cb141807
ragsub <raghavsk@CPX-ZSX3RXMQAFN.dir.svc.accenture.com>
2022-03-03 09:50:53 +0530

    change colors

--- Word/forms.py
+++ Word/forms.py
@@ -25,12 +25,12 @@ class WordleForm(forms.Form):
 
 class GuessForm(forms.Form):
     guess = forms.CharField(required=False, widget=forms.HiddenInput())
-    l1_color = forms.CharField(initial='btn-light',widget=forms.HiddenInput())
-    l2_color = forms.CharField(initial='btn-light',widget=forms.HiddenInput())
-    l3_color = forms.CharField(initial='btn-light',widget=forms.HiddenInput())
-    l4_color = forms.CharField(initial='btn-light',widget=forms.HiddenInput())
-    l5_color = forms.CharField(initial='btn-light',widget=forms.HiddenInput())
+    l1_color = forms.CharField(initial='bg-secondary bg-opacity-25 text-light',widget=forms.HiddenInput())
+    l2_color = forms.CharField(initial='bg-secondary bg-opacity-25 text-light',widget=forms.HiddenInput())
+    l3_color = forms.CharField(initial='bg-secondary bg-opacity-25 text-light',widget=forms.HiddenInput())
+    l4_color = forms.CharField(initial='bg-secondary bg-opacity-25 text-light',widget=forms.HiddenInput())
+    l5_color = forms.CharField(initial='bg-secondary bg-opacity-25 text-light',widget=forms.HiddenInput())
 
 
 class AlphabetForm(forms.Form):
-    l_color = forms.CharField(required=False, initial='bg-light', widget=forms.HiddenInput())
+    l_color = forms.CharField(required=False, initial='bg-secondary bg-opacity-25 text-light', widget=forms.HiddenInput())
